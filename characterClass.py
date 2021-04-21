2dacd7bdbde12c692fe494b68a54daf03aa2840b
day1127 <61472008+day1127@users.noreply.github.com>
2021-04-21 13:30:47 -0400

    changed more or less everything

--- characterClass.py
+++ characterClass.py
@@ -1,28 +1,45 @@
+import random
 
+def d6():
+    return random.randint(1, 6)
+
+def fourD6MinusLow():
+    rolls = []
+    for i in range(4):
+        rolls.append(d6())
+        rolls.sort(reverse = True)
+        rolls.pop()
+        return sum(rolls)
 
 class character:
 
-    def __init__(self, name, gender, Hp = 100, stamina = 100,  hight = 6,):
+    def __init__(self, name, Hp = 100, stamina = 100, charisma = 0, strength = 0, dexterity = 0):
         self.name = name
         self.Hp = Hp
         self.inventory = []
         self.stam = stamina
-        self.gender = gender
-        self.hight = hight
-        self.their = "their"
-        self.heOrShe = "they"
-        self.himOrHer = "them"
-
-    def __str__(self):
-        return f"{self.name} has {self.Hp} Hp, and {self.inventory} in {self.their} inventory, their gender is {self.gender}, and they are {self.hight} tall"
+        self.chr = charisma
+        self.str = strength
+        self.dex = dexterity
+        self.getStats()
 
 
 
-    def addToInv(self,item):
+    def addToInv(self, item):
         self.inventory.append(item)
 
-if __name__ == "__main__":
-    a = character("Jeff", "non-binary")
 
-    addToInv("sword")
+    def getStats(self):
+        self.chr = fourD6MinusLow()
+        self.str = fourD6MinusLow() + d6()
+        self.dex = fourD6MinusLow() + d6() + d6() - d6()
+
+
+    def __str__(self):
+        return f"{self.name} has {self.Hp} Hp, and {self.inventory} in their inventory, they have {self.chr} charisma, {self.str} strength, and {self.dex} dexterity"
+
+if __name__ == "__main__":
+    a = character("Jeff")
+
+    a.addToInv("sword")
     print (a.__str__())
